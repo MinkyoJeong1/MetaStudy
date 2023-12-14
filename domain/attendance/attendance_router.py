4e42158c6e87989b8f1e626fb83e6b7a1555aaa1
Minkyo <isakin.tyrfing@gmail.com>
2023-12-14 14:52:35 +0900

    FEAT: add attendance

--- domain/attendance/attendance_router.py
+++ domain/attendance/attendance_router.py
@@ -5,20 +5,27 @@ from starlette import status
 
 from database import get_db
 from domain.attendance import attendance_crud, attendance_schema
+from domain.user.user_router import get_current_user
+
+from models import User
 
 router = APIRouter(
     prefix="/api/attendance",
 )
 
 @router.get("/list", response_model=list[attendance_schema.Attendance])
-def question_list(db: Session = Depends(get_db)):
+def question_list(db: Session = Depends(get_db), 
+                  current_user: User = Depends(get_current_user)):
     _attendance_list = attendance_crud.get_attendance_list(db)
     return _attendance_list
 
 
-@router.post("/attendance", status_code=status.HTTP_204_NO_CONTENT)
-def check_attendance(_check_attendance: attendance_schema.Attendance, db: Session = Depends(get_db)):
-    user_exists = attendance_crud.get_existing_user(db, _check_attendance.username)
+@router.get("/attendance", status_code=status.HTTP_204_NO_CONTENT)
+def check_attendance(
+                     db: Session = Depends(get_db), 
+                     current_user: User = Depends(get_current_user)
+                     ):
+    user_exists = attendance_crud.get_existing_user(db, current_user.username)
     if not user_exists:
         raise HTTPException(status_code=400, detail="사용자가 존재하지 않습니다.")
-    attendance_crud.attendance_check(db=db, check_attendance=_check_attendance)
+    attendance_crud.attendance_check(db=db, check_attendance=current_user)
